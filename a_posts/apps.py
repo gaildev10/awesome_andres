9fca3055a35f79514c2305927182f671835b7862
GailDev10 <GailDev10@gmail.com>
2024-10-07 10:23:13 -0400

    Initial commit

---
+++ a_posts/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class APostsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'a_posts'
+
+    def ready(self):
+        import a_users.signals
